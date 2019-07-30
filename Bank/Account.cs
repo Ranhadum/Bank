c48a4eb60dcb65173e9e79474e79f733aa6c26e1
Ranhadum <szczepaniakszczep@gmail.com>
2019-07-30 23:20:19 +0200

    adding id #15

--- Bank/Account.cs
+++ Bank/Account.cs
@@ -8,18 +8,21 @@ namespace Bank
 {
     abstract class Account
     {
+        public int Id;
         public string AccountNumber;
         public decimal Balance;
         public string FirstName;
         public string LastName;
         public long Pesel;
 
-        public Account(string accountnumber, string firstname, string lastname, decimal balance, long pesel)
+
+        public Account(int id, string accountnumber, string firstname, string lastname, decimal balance, long pesel)
         {
-            AccountNumber = accountnumber;
+            Id = id;
+            AccountNumber = generateAccountNumber(id);
             FirstName = firstname;
             LastName = lastname;
-            Balance = balance;
+            Balance = 0,0M;
             Pesel = pesel;
         }
 
@@ -35,6 +38,13 @@ namespace Bank
         public string GetBalance()
         {
             return string.Format("{0}zł ", Balance);
+        }
+
+        private string generateAccountNumber(int id)
+        {
+            var accountNumber = String.Format("94{0:D10}", id);
+
+            return accountNumber;
         }
     }
 }
